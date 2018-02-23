1a0522a4f980ad1010244eb8fed476958527fdc1
Jade Gonzales <jadegonzales@Jades-MacBook-Pro.local>
2018-02-22 22:09:05 -0600

    cleaned up if/else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,161 +1,88 @@
-window.onload = function() {
-  gameinitalLoad();
-}
-var words = ["Dolphin", "Shark", "Turtle", "Jellyfish", "Eel", "Coral", "Barnacle", "Fish", "Mermaid"]
-
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-
-
-// variables that are global 
-
-
-var chosenWord = "";
-var chosenLetters = [];
-var guessedLetters = [];
-var spaces = 0;
-var incorrect = [];
-var letterguessed = "";
-
-
-var wins = 0;
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var animals = ["Dolphin", "Shark", "Turtle", "Jellyfish", "Eel", "Coral", "Barnacle", "Fish", "Mermaid"]
+var chosenWord; // Animal selected to be guessed
+var guessedLetter; // Currently guessed letter
+var lettersGuessed = []; // Array to store letters that have been guessed by user
 var losses = 0;
-var startingGuesses = 12
+var wins = 0;
+var startingGuesses = 12;
 
 
+window.onload = function () {
+    gameInitalLoad();
 
-function gameinitalLoad() { //starts the game
+}
 
-  var wordContainer = document.getElementById("words"); 
-
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-
-  chosenLetters = chosenWord.split("");
-
-
-  wordContainer.innerHTML = "";
-
-  //document.onkeyup = function(event)
-
-  document.getElementById("").innerHTML = startingGuesses;
-
-  document.getElementById("points").innerHTML = wins;
-  
-  document.getElementById("won").removeAttribute('style');
-
-  document.getElementById("losses").innerHTML = losses;
-
-
-
-
-  }
-
-  // document.getElementById("gameover").removeAttribute('style');
-
-
-  // chosenWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
-
-  chosenLetters = chosenWord.split(""); 
-  
-  guessedLetters = [];
-  
-
-  incorrect = []; 
-  
-
-  document.getElementById("incorrect").innerHTML = incorrect;
-
-
-  for ( i = 0; i < guessedLetters.length; i++ ) {
-
-    var tile = document.createElement("span");
-    tile.className = guessedLetters[i] + 'You Lost!';
-    if ( guessedLetters[i] == " " ) {
-      tile.className = "words"; 
-    } 
-    tile.innerHTML = "<b>" + chosenLetters[i] + "</b>";
-    wordContainer.appendChild(tile); 
-  } 
-} 
-
-
-
-function gameinitalLoad() {
-
-  document.onkeyup = function(event){
-
-  if(incorrect > 0) {
-    var event = window.event;
-    var inputLetter = event.key;
-
-    if(alphabet.indexOf(inputLetter) > -1) { 
-
-
-      var used = chosenLetters.indexOf(chosenLetter);
-
-    
-      if ( used === -1 ) {
-        chosenLetters.push(chosenLetter);
-  
-        var history = chosenLetters.join(" ");
-        document.getElementById("history").innerHTML = history;
-
-      
-        if (chosenLetters.indexOf(inputLetter) > -1 ) {
-          var spans = document.getElementsByClassName(chosenLetter);
-
-          for ( i = 0; i < spans.length; i++ ) {
-            var classes = inputLetter + "bar";
-            spans[i].className = classes;
-          } 
-
-         
-          var remainingLetters = document.getElementsByClassName("You Lost!");
-          if ( remainingLetters.length == 0 ) {
-            wins = wins + 1;
-            document.getElementById("Correct Lines").innerHTML = wins;
-
-        
-              document.getElementById("won").style.display = "block";
-              endGame();
-
-          }
-
-           } 
-        else {
-          incorrect = incorrect - 1;
-          document.getElementById("incorrect").innerHTML = incorrect;
-          if ( incorrect == 0 ) {
-           
-            document.getElementById("lost").style.display = "block";
-            losses = losses + 1;
-            document.getElementById("losses").innerHTML = losses;
-            endGame();
-
-          }
-        } // end else
-      }
-      }
+document.onkeyup = function (event) {
+    // If the key pushed is not a letter, prompt user to choose another letter
+    if (!alphabet.includes(event.key)) {
+        alert("Please select a letter a-z");
     }
-  }
+
+    // If the letter pushed was already guessed, prompt user to choose another letter
+    if (lettersGuessed.includes(event.key)) {
+        alert("You already guessed this letter");
+    }
+
+    // Else, add the letter to the lettersGuessed[]
+    else {
+        console.log(event.key);
+        lettersGuessed.push(event.key);
+        guessedLetter.innerText = lettersGuessed;
+    }
+}
+
+// Start of game
+function gameInitalLoad() {
+
+    // Used to hide the animal chosen
+    var hideWord = [];
+
+    // Test for displaying chosenWord on screen
+    var hidden = document.getElementById("guessedWord");
+
+    // Generate a random number between 0 and length of animals[]
+    var index = Math.floor(Math.random() * animals.length);
+
+    // set chosenWord to randomly selected animal
+    chosenWord = animals[index];
+    // console.log(chosenWord);
+
+    // get the id "guessedLetters from the index.html to display guessedLetter"
+    guessedLetter = document.getElementById("guessedLetters");
+
+    // Creating a loop to hide the letters of the selected animal from animals[]
+    for (var i = 0; i < chosenWord.length; i++) {
+        hideWord[i] = "-";
+    }
+
+    // Display dashes to screen
+    hidden.innerHTML = hideWord.join("");
 }
 
 
-function endGame() {
-  var counter = 3;
-  var countDown = document.getElementById("countDown");
-  countDown.innerHTML = "Start over! Next game starts in 3 seconds!";
-  var id;
 
 
-  id = setInterval(function() {
-      counter--;
-      if(counter < 0) {
-        countDown.innerHTML = '';
-        gameinitalLoad();
-        clearInterval(id);
-      } else {
-          countDown.innerHTML = "Next Round starts in " + counter.toString() + " seconds.";
-      }
-  }, 1000);
-}
+    // var wordContainer = document.getElementById("animals");
+
+    // chosenWord = animals[Math.floor(Math.random() * animals.length)];
+
+    // chosenLetters = chosenWord.split("");
+
+
+    // wordContainer.innerHTML = "";
+
+    // //document.onkeyup = function(event)
+
+    // document.getElementById("").innerHTML = startingGuesses;
+
+    // document.getElementById("points").innerHTML = wins;
+
+    // document.getElementById("won").removeAttribute('style');
+
+    // document.getElementById("losses").innerHTML = losses;
+
+
+
+
+
